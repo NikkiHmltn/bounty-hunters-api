9b8724fb9965f0b56d14cba6fb5470c66528b8d3
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-15 09:18:11 -0800

    initial setups and schema

---
+++ dbTest.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const models = require('./models')
+
+// models.Bounty.create({
+//     name: 'Yosemite Sam',
+//     wantedFor: 'Rootin & Tootin',
+//     clien: 'Walt Disney',
+//     reward: 1000,
+//     ship: 'good ship lollipop',
+//     hunters: ['Bugs Bunny'],
+//     captured: false,
+//     lastSeen: '1982'
+// })
